5e8f23ad0a3f06a9acae80b053ff38468e16df57
Gurnoor Singh Batth <gurnoorbatth10@gmail.com>
2022-01-10 22:12:58 +0530

    Final

---
+++ Game.xaml.cs
@@ -0,0 +1,124 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace TicTacToe_WPF
+{
+    /// <summary>
+    /// Interaction logic for Game.xaml
+    /// </summary>
+    public partial class Game : Window
+    {
+        string choice, player1, player2;
+        int x = 1;
+        Button clickBtn;
+        MessageBoxResult res;
+
+        string[,] vs = new string[3, 3] { { "1", "2", "3" }, { "4", "5", "6" }, { "7", "8", "9" } };
+        public Game()
+        {
+            InitializeComponent();
+        }
+
+        public Game(string choice, string player1, string player2)
+        {
+            InitializeComponent();
+            this.choice = choice;
+            this.player1 = player1;
+            this.player2 = player2;
+            txtBlock.Text = player1;
+        }
+
+
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+            clickBtn = sender as Button;
+
+            if (x % 2 != 0)
+            {
+                txtBlock.Text = player2;
+                proceedTurn();
+                choice = "O";
+            }
+            else
+            {
+                txtBlock.Text = player1;
+                proceedTurn();
+                choice = "X";
+            }
+            int dec = checkWin();
+            if (dec == 1)
+            {
+                declareWin();
+            }
+            else if (dec == -1)
+            {
+                res = MessageBox.Show("Match Tied", "Result", MessageBoxButton.OK);
+
+
+            }
+
+
+
+
+        }
+
+        public void proceedTurn()
+        {
+            // Displays the choice of the Player on the button clicked
+            clickBtn.Content = choice;
+            clickBtn.FontSize = 100;
+            clickBtn.Padding = new Thickness(-10);
+            clickBtn.IsEnabled = false;
+            vs[Grid.GetRow(clickBtn), Grid.GetColumn(clickBtn)] = choice;
+            x++;
+        }
+        public int checkWin()
+        {
+            int win = 0;
+            // Checking Horizontal Wins
+            if ((vs[0, 0] == vs[0, 1] && vs[0, 1] == vs[0, 2]) || (vs[1, 0] == vs[1, 1] && vs[1, 1] == vs[1, 2]) || (vs[2, 0] == vs[2, 1] && vs[2, 1] == vs[2, 2]))
+            {
+                return 1;
+
+            }
+            // Checking Vertical Wins
+            else if ((vs[0, 0] == vs[1, 0] && vs[1, 0] == vs[2, 0]) || (vs[0, 1] == vs[1, 1] && vs[1, 1] == vs[2, 1]) || (vs[0, 2] == vs[1, 2] && vs[1, 2] == vs[2, 2]))
+            {
+
+                return 1;
+            }
+            //checking Diagonal Wins
+            else if ((vs[0, 0] == vs[1, 1] && vs[1, 1] == vs[2, 2]) || (vs[2, 0] == vs[1, 1] && vs[1, 1] == vs[0, 2]))
+            {
+                return 1;
+            }
+            //checking for draw
+            else if (win == 0 && x == 10)
+            {
+                return -1;
+            }
+            else
+            {
+                return 0;
+            }
+
+
+        }
+
+        public void declareWin()
+        {
+            if ((x - 1) % 2 == 0)
+            {
+                MessageBox.Show(player2 + " Wins :)", "Result", MessageBoxButton.OK);
+            }
+            else
+            {
+                MessageBox.Show(player1 + " Wins :)", "Result", MessageBoxButton.OK);
+
+            }
+        }
+    }
+
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,92 @@
+﻿using System.Windows;
+
+namespace TicTacToe_WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            text.IsReadOnly = true;
+        }
+
+        string pOneName, pTwoName;
+
+
+        private void btnP1_Click(object sender, RoutedEventArgs e)
+        {
+            user1.Clear();
+        }
+
+        private void btnP2_Click(object sender, RoutedEventArgs e)
+        {
+            user2.Clear();
+        }
+
+
+
+        private void btnAll_Click(object sender, RoutedEventArgs e)
+        {
+            user1.Clear();
+            user2.Clear();
+            user1.IsReadOnly = false;
+            user2.IsReadOnly = false;
+
+        }
+
+
+        private void btnSubmit_Click(object sender, RoutedEventArgs e)
+        {
+            pOneName = user1.Text.ToString();
+            pTwoName = user2.Text.ToString();
+            if (string.IsNullOrWhiteSpace(pOneName) || string.IsNullOrWhiteSpace(pTwoName))
+            {
+                MessageBox.Show("Player Names cannot be Empty or Whitespaces. Please Enter Valid Names", "Error", MessageBoxButton.OK,MessageBoxImage.Error);
+            }
+            else
+            {
+                user1.IsReadOnly = true;
+
+                user2.IsReadOnly = true;
+                btnAll.IsEnabled = false;
+                btnP1.IsEnabled = false;
+                btnP2.IsEnabled = false;
+                showChoice("X");
+            }
+        }
+
+        private void btnReset_Click(object sender, RoutedEventArgs e)
+        {
+            user1.Clear();
+            user2.Clear();
+            user1.IsReadOnly = false;
+            user2.IsReadOnly = false;
+            btnAll.IsEnabled = true;
+            btnP1.IsEnabled = true;
+            btnP2.IsEnabled = true;
+        }
+
+        public void showChoice(string choose)
+        {
+            MessageBoxResult Confirm = MessageBox.Show("Do you want to proceed with your choice?", "Proceed", MessageBoxButton.YesNo,MessageBoxImage.Exclamation);
+
+            if (Confirm == MessageBoxResult.Yes)
+            {
+                Game game1 = new Game(choose, pOneName, pTwoName);
+                game1.Show();
+            }
+            else
+            {
+                btnAll.IsEnabled = true;
+                btnP1.IsEnabled = true;
+                btnP2.IsEnabled = true;
+            }
+
+        }
+
+
+    }
+}
